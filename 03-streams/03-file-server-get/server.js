07f1760b4d6827a073e54b18a00390dcc5f1dc57
Ivan Khomenko <khomenko.ivan@gmail.com>
2022-05-18 00:02:41 +0300

    03 streams 03 file server get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,29 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Wrong path');
+    return;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
+  console.log(filepath);
 
   switch (req.method) {
     case 'GET':
-
+      const file = fs.createReadStream(filepath);
+      file.pipe(res);
+      file.on('error', () => {
+        res.statusCode = 404;
+        res.end('No file');
+      });
       break;
 
     default:
